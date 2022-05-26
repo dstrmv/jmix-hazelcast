d62de927b6c53a9e78191c673bd46adfb7b45d04
Ivan Bulychev <i.bulychev@haulmont.com>
2022-05-26 22:50:07 +0400

    Initial commit

---
+++ src/main/java/ivan/dummy/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package ivan.dummy.screen.user;
+
+import ivan.dummy.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/ivan/dummy/JmixHazelcastApplicationTests.java
@@ -0,0 +1,13 @@
+package ivan.dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmixHazelcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
